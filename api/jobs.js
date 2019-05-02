156e915395d2e4e6bb1517afdeda7c51858623ec
M1kaChu311 <micaeljorg3@gmail.com>
2019-05-02 14:42:36 +0300

    changes

--- api/jobs.js
+++ api/jobs.js
@@ -1,27 +1,26 @@
 const db = require("./db");
 
 function uploadjob(name, description, callback) {
-  let query = `INSERT INTO finalprojectdb.jobs (jobname,jobdescription) VALUES(?,?);`;
-  db.runQuery(query, [name, description], callback);
+  let query = `INSERT INTO finalprojectdb.jobs (jobname,jobdescription,publishdate,stillrelevant) VALUES(?,?,?,?);`;
+  db.runQuery(query, [name, description, new Date(), true], callback);
 }
 
 function getjobs(callback) {
   let query = "SELECT * FROM finalprojectdb.jobs;";
   db.runQuery(query, [], callback);
 }
-function updatejob(name, id, callback) {
-  let query = `UPDATE finalprojectdb.jobs set name=? WHERE id=?;`;
-  db.runQuery(query, [name, id], callback);
+function updatejob(name, description, id, callback) {
+  let query = `UPDATE finalprojectdb.jobs set jobname=?,jobdescription=? WHERE jobid=?;`;
+  db.runQuery(query, [name, description, id], callback);
 }
 
 function deletejob(id, callback) {
-  let query = `DELETE FROM finalprojectdb.jobs WHERE id=?;`;
+  let query = `UPDATE finalprojectdb.jobs set stillrelevant = 0 WHERE jobid=?;`;
   db.runQuery(query, [id], callback);
 }
 
 module.exports = {
   getjobs: getjobs,
-  getjobByID: getjobByID,
   uploadjob: uploadjob,
   deletejob: deletejob,
   updatejob: updatejob
--- api/server.js
+++ api/server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const jobsFunction = require("./jobs.js");
+
 const bodyParser = require("body-parser");
+
 app.use(
   bodyParser.urlencoded({
     extended: false
   })
 );
 app.use(bodyParser.json());
-const jobsFunction = require("./jobs.js");
 
 app.post("/jobs", (req, res) => {
   if (
@@ -17,20 +19,78 @@ app.post("/jobs", (req, res) => {
     typeof req.body.description == "undefined" ||
     req.body.description == ""
   ) {
-    res.status(404).send({ message: "bad request" });
+    res.status(400).send({ message: "bad request" });
   } else {
-    booksFunction.uploadjob(
+    jobsFunction.uploadjob(req.body.name, req.body.description, (err, data) => {
+      if (err) {
+        res.status(500).send({ error: err.code });
+      } else {
+        res.send({
+          message: `job with id ${data.insertId} uploaded successfuly`
+        });
+      }
+    });
+  }
+});
+
+app.get("/jobs", (req, res) => {
+  jobsFunction.getjobs(function(err, books) {
+    if (err) {
+      res.status(400).send(err);
+    } else {
+      res.send(books);
+    }
+  });
+});
+
+app.delete("/jobs", (req, res) => {
+  if (typeof req.body.id == "undefined" || req.body.id == "") {
+    res.status(400).send({ message: "bad request" });
+  } else {
+    jobsFunction.deletejob(req.body.id, (err, data) => {
+      if (err) {
+        res.status(500).send({ error: err.code });
+      } else {
+        if (data.affectedRows == 0) {
+          res
+            .status(400)
+            .send({ message: `no job with with id ${req.body.id}` });
+        } else {
+          res.send({
+            message: `job with id ${req.body.id} deleted successfuly`
+          });
+        }
+      }
+    });
+  }
+});
+
+app.put("/jobs", (req, res) => {
+  if (
+    typeof req.body.id == "undefined" ||
+    req.body.id == "" ||
+    typeof req.body.name == "undefined" ||
+    req.body.name == "" ||
+    typeof req.body.description == "undefined" ||
+    req.body.description == ""
+  ) {
+    res.status(400).send({ message: "bad request" });
+  } else {
+    jobsFunction.updatejob(
       req.body.name,
       req.body.description,
+      req.body.id,
       (err, data) => {
         if (err) {
           res.status(500).send({ error: err.code });
         } else {
-          res.send({
-            message: `המשרה ${req.body.name} הועלתה בהצלחה עם מזהה ${
-              data.insertId
-            }`
-          });
+          if (data.affectedRows == 0) {
+            res.send({ message: `no job with id ${req.body.id} ` });
+          } else {
+            res.send({
+              message: `job with id ${req.body.id} updated successfuly`
+            });
+          }
         }
       }
     );
