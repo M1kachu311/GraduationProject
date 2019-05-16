e40f031496ebd7cae7cd156cdebdb4e921087298
M1kaChu311 <micaeljorg3@gmail.com>
2019-05-16 15:24:54 +0300

    connected to api

--- graduation-project/src/components/simpleComponents/ExtensionPanelList.js
+++ graduation-project/src/components/simpleComponents/ExtensionPanelList.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react'
-import ExtensionPanel from '../simpleComponents/ExtensionPanel'
+import React, { Component } from "react";
+import ExtensionPanel from "../simpleComponents/ExtensionPanel";
 
 export class ExtensionPanelList extends Component {
+  componentDidMount() {
+    fetch("http://127.0.0.1:3002/jobs")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({ data: myJson });
+      });
+  }
+  state = {
+    data: []
+  };
 
-    state = { 
-        data: [
-            {
-                Date:'01/02/1994', 
-                ID:1, Name:'Developper', 
-                Description:'Lorem ipsum', 
-                Type:'full time job', 
-                Location:'Yeruham'
-            }
-        ] 
-    };  
-
-    render() {
+  render() {
     return this.state.data.map(job => {
-        return (
-                <ExtensionPanel 
-                    key={job.ID} 
-                    date={job.Date} 
-                    name={job.Name} 
-                    description={job.Description}
-                    type={job.Type}
-                    location={job.Location}
-                />
-        )
-    })
-    }
+      return (
+        <ExtensionPanel
+          key={job.ID}
+          date={job.Date}
+          name={job.Name}
+          description={job.Description}
+          type={job.Type}
+          location={job.Location}
+        />
+      );
+    });
+  }
 }
 
-export default ExtensionPanelList
+export default ExtensionPanelList;
--- graduation-project/src/components/simpleComponents/HomeCards.js
+++ graduation-project/src/components/simpleComponents/HomeCards.js
@@ -1,55 +1,49 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import { Link } from 'react-router-dom'
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import { Link } from "react-router-dom";
 
 const styles = theme => ({
-    card: {
-        maxWidth: 400,
-        width: 400,
-        height: 400,
-        margin: '20px'
-    }
+  card: {
+    maxWidth: 400,
+    width: 400,
+    height: 400,
+    margin: "20px",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center"
+  }
 });
 
-class RecipeReviewCard extends React.Component {
-    state = { expanded: false };
+class HomeCards extends React.Component {
+  state = { expanded: false };
 
-    handleExpandClick = () => {
-        this.setState(state => ({ expanded: !state.expanded }));
-    };
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
 
-    render() {
-        const { classes } = this.props;
+  render() {
+    const { classes } = this.props;
 
-        return (
-            <Card className={classes.card}>
-            <CardHeader
-            title={this.props.title}
-            subheader={this.props.subtitle}
-            />
-            <CardMedia
-            //   className={classes.media} need our own css here
-            image={this.props.picture}
-            title={this.props.picTitle}
-            />
-            <CardContent
-            // here will sit the component for the jobs
-            >
-            
-            </CardContent>
-            <div><Link to={this.props.link}>לוח משרות</Link></div>
-            </Card>
-        );
-    }
+    return (
+      <Card className={classes.card}>
+        <CardHeader title={this.props.title} />
+        {this.props.linkDescription}
+        <div>
+          <Link to={this.props.link}>{this.props.linkText}</Link>
+        </div>
+      </Card>
+    );
+  }
 }
 
-RecipeReviewCard.propTypes = {
-    classes: PropTypes.object.isRequired,
+HomeCards.propTypes = {
+  classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(RecipeReviewCard);
+export default withStyles(styles)(HomeCards);
