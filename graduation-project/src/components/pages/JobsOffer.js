ffa71be5ddf06733c9b11119c6a22b862f56b242
M1kaChu311 <micaeljorg3@gmail.com>
2019-05-16 21:14:48 +0300

    changes

--- graduation-project/src/components/pages/JobsOffer.js
+++ graduation-project/src/components/pages/JobsOffer.js
@@ -1,44 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExtensionPanelListForJobs from '../simpleComponents/ExtensionPanelListForJobs'
-import IhaveAJobPopUp from '../simpleComponents/IhaveAJobPopUp'
+import ExtensionPanelListForJobs from "../simpleComponents/ExtensionPanelListForJobs";
+import IhaveAJobPopUp from "../simpleComponents/IhaveAJobPopUp";
 
-const date ='תאריך'
-const office = 'משרה'
-const officeKind = 'סוג המשרה'
-const location = 'מיקום'
+const date = "תאריך";
+const office = "משרה";
+const officeKind = "סוג המשרה";
+const location = "מיקום";
 
 const optionsStyle = {
-    display: 'flex',
-    justifyContent: 'space-around'
-}
+  display: "flex",
+  justifyContent: "space-around"
+};
 
 const mainStyle = {
-	width: '85%',
-	margin: '0 auto'
-}
+  width: "90%",
+  minHeight: "500px",
+  margin: "20px auto"
+};
 
 const optionsTitleStyle = {
-    // margin: '10px 70px',
-}
+  // margin: '10px 70px',
+};
 
 export class JobsOffer extends Component {
-
   render() {
     return (
-        <div style={mainStyle}>
-            <div style={optionsStyle}>
-                <p style={optionsTitleStyle}>{date}</p>
-                <p style={optionsTitleStyle}>{office}</p>
-                <p style={optionsTitleStyle}>{officeKind}</p>
-                <p style={optionsTitleStyle}>{location}</p>
-                <IhaveAJobPopUp />
-            </div>
-            <ExtensionPanelListForJobs />
+      <div style={mainStyle}>
+        <div style={optionsStyle}>
+          <p style={optionsTitleStyle}>{date}</p>
+          <p style={optionsTitleStyle}>{office}</p>
+          <p style={optionsTitleStyle}>{officeKind}</p>
+          <p style={optionsTitleStyle}>{location}</p>
+          <IhaveAJobPopUp />
         </div>
-        
-    )
+        <ExtensionPanelListForJobs />
+      </div>
+    );
   }
 }
 
-export default JobsOffer
+export default JobsOffer;
--- graduation-project/src/components/pages/RealEstate.js
+++ graduation-project/src/components/pages/RealEstate.js
@@ -1,50 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ExtensionPanelListForRealEstate from '../simpleComponents/ExtensionPanelListForRealEstate'
-import IhaveAJobPopUp from '../simpleComponents/IhaveAJobPopUp'
+import ExtensionPanelListForRealEstate from "../simpleComponents/ExtensionPanelListForRealEstate";
+import IhaveAJobPopUp from "../simpleComponents/IhaveAJobPopUp";
 
-const salleOrRent = 'מכירה/השכרה'
-const type = 'סוג הנכס'
-const rooms = 'מס׳ חדרים'
-const floor = 'קומה'
-const adrdess = 'כתובת'
-const price = 'מחיר'
-const date = 'תאריך'
+const salleOrRent = "מכירה/השכרה";
+const type = "סוג הנכס";
+const rooms = "מס׳ חדרים";
+const floor = "קומה";
+const adrdess = "כתובת";
+const price = "מחיר";
+const date = "תאריך";
 
 const optionsStyle = {
-    display: 'flex',
-    justifyContent: 'space-around'
-}
+  display: "flex",
+  justifyContent: "space-around"
+};
 
 const mainStyle = {
-	width: '85%',
-	margin: '0 auto'
-}
+  width: "90%",
+  minHeight: "500px",
+  margin: "20px auto"
+};
 
 const optionsTitleStyle = {
-    // margin: '10px 70px',
-}
+  // margin: '10px 70px',
+};
 
 export class RealEstate extends Component {
-
   render() {
     return (
-        <div style={mainStyle}>
-            <div style={optionsStyle}>
-                <p style={optionsTitleStyle}>{salleOrRent}</p>
-                <p style={optionsTitleStyle}>{type}</p>
-                <p style={optionsTitleStyle}>{rooms}</p>
-                <p style={optionsTitleStyle}>{floor}</p>
-                <p style={optionsTitleStyle}>{adrdess}</p>
-                <p style={optionsTitleStyle}>{price}</p>
-                <p style={optionsTitleStyle}>{date}</p>
-                <IhaveAJobPopUp />
-            </div>
-            <ExtensionPanelListForRealEstate />
+      <div style={mainStyle}>
+        <div style={optionsStyle}>
+          <p style={optionsTitleStyle}>{salleOrRent}</p>
+          <p style={optionsTitleStyle}>{type}</p>
+          <p style={optionsTitleStyle}>{rooms}</p>
+          <p style={optionsTitleStyle}>{floor}</p>
+          <p style={optionsTitleStyle}>{adrdess}</p>
+          <p style={optionsTitleStyle}>{price}</p>
+          <p style={optionsTitleStyle}>{date}</p>
+          <IhaveAJobPopUp />
         </div>
-        
-    )
+        <ExtensionPanelListForRealEstate />
+      </div>
+    );
   }
 }
 
-export default RealEstate
+export default RealEstate;
