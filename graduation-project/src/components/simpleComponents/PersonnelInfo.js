2a4f0b352ca9190d6d3ec4c4b13c5e4c178580fb
M1kaChu311 <micaeljorg3@gmail.com>
2019-05-16 11:32:11 +0300

    combined with cms

--- graduation-project/src/components/simpleComponents/PersonnelInfo.js
+++ graduation-project/src/components/simpleComponents/PersonnelInfo.js
@@ -1,65 +1,73 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Paper from '@material-ui/core/Paper'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Paper from "@material-ui/core/Paper";
 
-import AvatarComp from './AvatarComp'
+import AvatarComp from "./AvatarComp";
 
 const titlesStyle = {
-    display: 'flex',
-    flexDirection: 'column'
-}
+  display: "flex",
+  flexDirection: "column"
+};
 
 const paperStyle = {
-    display: 'flex'
-}
+  display: "flex"
+};
 
 const titles = {
-    name: 'שם: ',
-    title: 'תפקיד: ',
-    phone: 'טלפון: ',
-    email: 'דוא״ל: '
-}
+  name: "שם: ",
+  title: "תפקיד: ",
+  phone: "טלפון: ",
+  email: "דוא״ל: "
+};
 
 export class PersonnelInfo extends Component {
+  state = { data: [] };
 
-    state = {data: [
-        {
-            Name: "Micael Jorge",
-            Title: "Web master",
-            Description: "Web master",
-            Image: "https://avatars0.githubusercontent.com/u/17184992?s=460&v=4",
-            Email: "micael@mail.com",
-            Phone: "0403254873",
-            ID: 1 
-        }
-    ]}
+  componentDidMount() {
+    fetch("http://127.0.0.1:3002/mainpersonnel")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({ data: myJson });
+      });
+  }
 
-    render() {
-
-        return (
-            this.state.data.map((person) => {
-                return (
-                    <div key={person.ID}>
-                        <Paper elevation={1} style={paperStyle}>
-                        <AvatarComp img={person.Image}/>
-                        <div style={titlesStyle}>
-                            <p>{titles.name}{person.Name}</p>
-                            <p>{titles.title}{person.Title}</p>
-                        </div>
-                        <div style={titlesStyle}>
-                            <p>{titles.phone}{person.Phone}</p>
-                            <p>{titles.email}{person.Email}</p>
-                        </div>
-                        </Paper>
-                    </div>
-                )
-            })
-        )
-    }
+  render() {
+    return this.state.data.map(person => {
+      return (
+        <div key={person.ID}>
+          <Paper elevation={1} style={paperStyle}>
+            <AvatarComp img={person.Image} />
+            <div style={titlesStyle}>
+              <p>
+                {titles.name}
+                {person.Name}
+              </p>
+              <p>
+                {titles.title}
+                {person.Title}
+              </p>
+            </div>
+            <div style={titlesStyle}>
+              <p>
+                {titles.phone}
+                {person.Phone}
+              </p>
+              <p>
+                {titles.email}
+                {person.Email}
+              </p>
+            </div>
+          </Paper>
+        </div>
+      );
+    });
+  }
 }
 
 PersonnelInfo.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
-export default (PersonnelInfo);
+export default PersonnelInfo;
