4af27cfb6ee1f8704d6bda1ec94d076095389d2f
Angot Nathan <goelnathan@gmail.com>
2019-05-30 13:17:10 +0300

    responsive

--- graduation-project/src/components/pages/Home.js
+++ graduation-project/src/components/pages/Home.js
@@ -12,32 +12,36 @@ const mainStyle = {
   justifyContent: "space-evenly",
   alignItems: "space-around",
   width: "100%",
-  height: "520px",
+  height: "100%",
   color: "#FFF"
 };
 
-const jobSectionStyle = {
-  width: "100%",
-  height: "250px",
-  display: "flex",
-  flexDirection: "row",
-  justifyContent: "space-around"
-};
+const SectionStyle = styled.div `
+  width: 100%;
+  height: 250px;
+  display: flex;
+  margin: 3px 0;
+  flex-direction: row;
+  justify-content: space-around;
+  @media only screen and (max-width: 540px) {
+      height: 200px
+  }
+`;
 
-const jobTitleStyle = {
-  width: "59.5%",
-  backgroundColor: "#FFF",
-  color: "rgb(52, 58, 64)",
-  fontSize: "20px",
-  border: "1px solid rgb(99, 174, 164)",
-  borderRadius: "5px",
-  padding: "40px",
-  boxSizing: "border-box",
-  textAlign: "center"
-};
+const TextSquareStyle = styled.div `
+  width: 59.5%;
+  background-color: #FFF;
+  color: rgb(52, 58, 64);
+  font-size: 20px;
+  border: 1px solid rgb(99, 174, 164);
+  border-sadius: 5px;
+  padding: 40px;
+  box-sizing: border-box;
+  text-align: center;
+`;
 const imgContainer = {
   width: "39%",
-  height: "250px"
+  height: "100%"
 };
 const imgContainerMB = {
   width: "100%",
@@ -48,43 +52,54 @@ const imgContainerTB = {
   height: "250px"
 };
 
-const jobTableStyle = {
-  width: "100%",
-  height: "100%",
-  backgroundColor: "#63AEA4",
-  display: "flex",
-  justifyContent: "center",
-  borderRadius: "5px",
-  alignItems: "center",
-  objectFit: "cover"
-};
-const aptTableStyle = {
-  width: "100%",
-  height: "100%",
-  backgroundColor: "#CADB30",
-  display: "flex",
-  justifyContent: "center",
-  borderRadius: "5px",
-  alignItems: "center",
-  objectFit: "cover"
-};
-const colorBlue = {
-  color: "rgb(99, 174, 164)",
-  textAlign: "center",
-  fontSize: "30px",
-  margin: "10px"
-};
+const AptTableStyle = styled.img`
+  width: 100%;
+  height: 100%;
+  background-color: #CADB30;
+  display: flex;
+  justify-content: center;
+  border-radius: 5px;
+  align-items: center;
+  object-fit: cover;
+`;
+
 const marginBtn = {
   margin: "20px",
   border: " 1px solid rgb(99, 174, 164)",
-  color: "  rgb(99, 174, 164)"
+  color: "  rgb(99, 174, 164)",
+  width: '130px',
+  display: 'flex',
 };
 
 const FirstSquareText = styled.p`
+  font-size: 1.5vw;
   @media (max-width: 540px) {
     display: none !important;
   }
 `;
+
+const ButtonLink = styled.a`
+  textDecoration: none;
+  display: flex;
+  justify-content: center;
+`;
+
+const ButtonText = styled.p`
+  @media (max-width: 540px) {
+    font-size: 10px;
+  }
+`;
+
+const TitleText = styled.p`
+  color: rgb(99, 174, 164);
+  text-align: center;
+  font-size: 30px;
+  margin: 10px;
+  @media (max-width: 540px) {
+    font-size: 20px;
+  }
+`;
+
 
 export class Home extends Component {
   render() {
@@ -93,9 +108,9 @@ export class Home extends Component {
         <ImagesCarousel />
 
         <div style={mainStyle}>
-          <div style={jobSectionStyle}>
-            <div style={jobTitleStyle}>
-              <p style={colorBlue}>מחפש עבודה?</p>
+          <SectionStyle>
+            <TextSquareStyle>
+              <TitleText>מחפש עבודה?</TitleText>
               <FirstSquareText>
                 מרכז הצעירים בנה עבור דורשי העבודה בנגב לוח משרות מגוון ועדכני
               </FirstSquareText>
@@ -104,24 +119,24 @@ export class Home extends Component {
                 שלח את פרטיך וקורות החיים שלך לרכזת התעסוקה שלנו על מנת להגביר
                 את סיכויי ההשמה{" "}
               </FirstSquareText>
-              <Link to="/jobs" style={{ textDecoration: "none" }}>
-                <Button variant="outlined" size="large" style={marginBtn}>
-                  ללוח משרות
+              <ButtonLink to="/jobs">
+                <Button variant="outlined" size="medium" style={marginBtn}>
+                  <ButtonText>ללוח משרות</ButtonText>
                 </Button>
-              </Link>
-            </div>
+              </ButtonLink>
+            </TextSquareStyle>
             <div style={imgContainer}>
-              <img style={jobTableStyle} src={jobImg} alt="job" />
+              <AptTableStyle src={jobImg} alt="job" />
             </div>
-          </div>
+          </SectionStyle>
 
-          <div style={jobSectionStyle}>
+          <SectionStyle>
             <div style={imgContainer}>
               {" "}
-              <img style={aptTableStyle} src={aptImg} alt="apartment " />
+              <AptTableStyle src={aptImg} alt="apartment" />
             </div>
-            <div style={jobTitleStyle}>
-              <p style={colorBlue}>רוצה לגור בירוחם?</p>
+            <TextSquareStyle>
+              <TitleText>רוצה לגור בירוחם?</TitleText>
               <FirstSquareText>
                 מרכז הצעירים אסף בשבילך את כל הדירות המוצעות להשכרה ולמכירה
                 בירוחם.
@@ -129,13 +144,13 @@ export class Home extends Component {
               <FirstSquareText>
                 השאירו את פרטיכם בטופס יצירת הקשר ונחבר אתכם לכל מה שקורה פה...
               </FirstSquareText>
-              <Link to="/realEstate" style={{ textDecoration: "none" }}>
-                <Button variant="outlined" size="large" style={marginBtn}>
-                  ללוח דיור
+              <ButtonLink to="/realEstate">
+                <Button variant="outlined" size="medium" style={marginBtn}>
+                  <ButtonText>ללוח דיור</ButtonText>
                 </Button>
-              </Link>
-            </div>
-          </div>
+              </ButtonLink>
+            </TextSquareStyle>
+          </SectionStyle>
         </div>
       </div>
     );
